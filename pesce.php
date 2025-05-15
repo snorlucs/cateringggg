c73bfb29b7850b40ba2dbb09d3a8e8f8ee0b6ef5
snorlucs <simonepagano8476@gmail.com>
2025-05-15 17:53:46 +0200

    aggiunta file per catering

---
+++ pesce.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>Elenco Menù</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>Elenco Tutti i Menù</h1>
+
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+ 
+$servername = "localhost";
+$username = "masterchef";
+$password = "123qwe";
+$dbname = "DBCatering";
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+ 
+// Check connection
+if ($conn->connect_errno) {
+  echo("Impossibile connettersi: " . $conn->connect_error ."\n");
+  exit();
+}
+
+$sql = "SELECT * FROM Menu where Nome = 'pesce' ";
+$result = $conn->query($sql);
+ 
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "Cod_M: " . $row["Cod_M"]. " - Nome: " . $row["Nome"] . " - Prezzo: " . $row["Prezzo"] . "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+$conn->close();
+
+?>
+</div>
+</body>
+</html>
